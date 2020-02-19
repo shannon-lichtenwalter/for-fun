f246d54f894ce0d8f494346d9f764045c8539c1d
Shannon Lichtenwalter <shannon.lichtenwalter@gmail.com>
2020-02-18 17:52:24 -0700

    messing around

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import sharky from './shark.png';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  state = {
+    shark: null
+  }
+
+  hoverAction = (e) => {
+    this.setState({
+      shark: e.target.className
+    })
+  }
+
+  renderBabyShark = () => {
+    let doo= 'doo '.repeat(6);
+    let sharks = ['baby', 'mommy', 'daddy', 'grandma', 'gandpa', 'lady swimmin\'']
+    return sharks.map((shark, index) => {
+      let sharkLine = shark === 'lady swimmin\'' ? shark + ' ' + doo : shark + ' shark ' + doo;
+      return (
+        <div onMouseOver={(e) => this.hoverAction(e)} className={shark + ' container'} key={index} id={this.state.shark === shark ? 'selected' : null}>
+        <div id='song' className={shark}>
+        <p className={shark}>{sharkLine}</p>
+        <p className={shark}>{sharkLine}</p>
+        <p className={shark}>{sharkLine}</p>
+        <p className={shark}>{shark === 'lady swimmin\'' ? 'CHOMP' : shark + ' shark! '}</p>
+        </div>
+        {this.state.shark === shark ? <img className='sharky' src={sharky} alt='shark'/> : null}
+        </div>
+      )
+    })
+  }
+  
+  render() {
+    return (
+      <main className='App'>
+        <h1>Baby Shark</h1>
+        {this.renderBabyShark()}
+    </main>
+    );
+  }
+
 }
 
 export default App;
